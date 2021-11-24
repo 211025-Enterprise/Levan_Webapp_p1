5599534d94bd0b277d353b19ac5dbcbedf746d44
levansiradze <levansiradze@yahoo.com>
2021-11-24 11:41:05 -0500

    update/delete not implemented

--- src/main/java/service/servletService.java
+++ src/main/java/service/servletService.java
@@ -2,15 +2,17 @@ package service;
 
 import Persistence.genericDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class servletService {
-
     public static void readAll(HttpServletRequest req, HttpServletResponse resp, Class<?> clazz) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         genericDAO orm = new genericDAO();
@@ -27,11 +29,12 @@ public class servletService {
         ObjectMapper mapper = new ObjectMapper();
         genericDAO orm = new genericDAO();
         boolean valueIn = false;
-        Object obj = new Object();
+        Object obj = null;
 
         if (cType.equals("application/json")){
             try{
                 obj = mapper.readValue(body, clazz);
+                System.out.println(obj.toString());
                 orm.create(clazz,obj);
                 valueIn = true;
                 resp.getWriter().println("Created table/added record");
@@ -51,9 +54,55 @@ public class servletService {
         }
     }
 
-    public static void updateTable(HttpServletRequest req, HttpServletResponse resp, Class<?> clazz) {
+    public static void updateTable(HttpServletRequest req, HttpServletResponse resp, Class<?> clazz) throws IOException {
+//        String content = req.getHeader("Content-Type");
+//        String body = req.getReader().lines().collect(Collectors.joining("\n"));
+//        String pk = req.getParameter("id");
+//        ObjectMapper mapper = new ObjectMapper();
+        genericDAO orm = new genericDAO();
+//        boolean updated = false;
+//        Object obj = null;
+//
+//        if (content.equals("application/json")){
+//            try {
+//                obj = mapper.readValue(body, clazz);
+//                System.out.println(obj);
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+        Map<String,String[]> parameterInput = req.getParameterMap();
+        List<Object> vals = new ArrayList<>();
+        List<Object> newvals = new ArrayList<>();
+        List<Field> cols = new ArrayList<>();
+        for (String k : parameterInput.keySet()){
+            try{
+                Field f = clazz.getDeclaredField(k);
+                vals.add(parameterInput.get(k)[0]);
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        }
+        Field[] fields = new Field[cols.size()];
+        ObjectMapper mapper = new ObjectMapper();
+        String body = req.getReader().lines().collect(Collectors.joining("\n"));
+        Object obj = null;
+        try{
+            obj = mapper.readValue(body, clazz);
+        }catch (IOException e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+            return;
+        }
+        for (int i =0;i<clazz.getDeclaredFields().length;i++){
+            newvals.add(fields[i]);
+        }
+        boolean updated = orm.update(clazz, vals.toArray(), cols.toArray(fields), newvals.toArray(), cols.toArray(fields));
+        resp.getWriter().println(updated);
+
     }
 
     public static void deleteTable(HttpServletRequest req, HttpServletResponse resp, Class<?> clazz) {
+
     }
 }
